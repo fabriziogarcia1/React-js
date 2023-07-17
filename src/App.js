d9e9aa86d12f5860bc4874d7f60d53569e822605
Fabri Garcia <fabriziocontra@gmail.com>
2023-07-17 20:19:09 -0300

    header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { navbar } from './componentes/navbar';
+
+
+function App() {
+  return (
+   <div>
+    {navbar}
+   </div>
+  );
+};
+console.log(navbar)
+export default App;
